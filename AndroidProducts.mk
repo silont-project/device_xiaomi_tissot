99f9bca6ed91951e269d36fb7de9557f84757086
Busetdah <apriantowpj@gmail.com>
2020-08-17 19:08:20 +0700

    Changes to ancient

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_tissot.mk
+    $(LOCAL_DIR)/ancient_tissot.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_tissot-user \
-    derp_tissot-userdebug \
-    derp_tissot-eng \
+    ancient_tissot-user \
+    ancient_tissot-userdebug \
+    ancient_tissot-eng \
